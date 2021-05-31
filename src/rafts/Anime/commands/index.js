d4db35efa5d585a70599f155623c81860a445dd2
Markens123 <mypepe17@gmail.com>
2021-05-31 11:14:04 -0400

    Removed anime alerts

--- src/rafts/Anime/commands/index.js
+++ src/rafts/Anime/commands/index.js
@@ -5,6 +5,5 @@ const commands = {};
 commands.link = require('./link');
 commands.mal = require('./mal');
 commands.sauce = require('./sauce');
-commands.animealerts = require('./animealerts');
 
 module.exports = commands;
