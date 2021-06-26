1c148937e32403e08bac3eb56386df8e634359a4
Markens123 <mypepe17@gmail.com>
2021-06-26 02:22:13 -0400

    Reverted back to regular slash cmd flag

--- src/rafts/portAuthority/commands/reloadcommand.js
+++ src/rafts/portAuthority/commands/reloadcommand.js
@@ -15,35 +15,36 @@ class ReloadCommandCommand extends BaseCommand {
   }
 
   async run(message, args) {
-    let interaction = false;
+    let slashcmd = false;
     if (!args) return message.channel.send(`Usage: ${this.boat.prefix}reloadcommand <Raft> <Command> (--slashcmd)`);
 
-    if (args.includes('--interaction') || args.includes('-i')) {
-      const index = args.indexOf('--interaction') > -1 ? args.indexOf('--interaction') : args.indexOf('-i');
-      interaction = args[index + 1];
-      args.splice(index, 2);
+    if (args.includes('--slashcmd') || args.includes('-s')) {
+      const index = args.indexOf('--slashcmd') > -1 ? args.indexOf('--slashcmd') : args.indexOf('-s');
+      slashcmd = true;
+      args.splice(index, 1);
     }
     const raftName = args[0];
     const commandName = args[1]?.toLowerCase();
-    let basepath = interaction ? this.boat.rafts[raftName]?.interactions[interaction] : this.boat.rafts[raftName].commands 
     
-    if (!basepath?.get(commandName)) return message.channel.send('The command and/or raft that have provided are invalid!');
+    if (!this.boat.rafts[raftName]?.commands.get(commandName) && !slashcmd) return message.channel.send('The command and/or raft that have provided are invalid!');
+    if (!this.boat.rafts[raftName]?.interactions?.commands?.get(commandName) && slashcmd) return message.channel.send('The command and/or raft that have provided are invalid!');
     
     const options = {
       cwd: `${__dirname}../../../`,
       realpath: true
     }
-    let path = glob.sync(`**${interaction ? `/interactions/${interaction}` : '/commands'}/${commandName}.js`, options)[0];
+    let path = glob.sync(`**${slashcmd ? '/interactions': ''}/commands/${commandName}.js`, options)[0];
     delete require.cache[require.resolve(path)];
     
     try {
       const raft = this.boat.rafts[raftName];
       const command = require(path);
-      basepath.set(commandName, new command(raft));
+      slashcmd ? raft.interactions.commands.set(commandName, new command(raft)) : 
+        raft.commands.set(commandName, new command(raft));
       
-      interaction ? this.boat.interactions[interaction].set(commandName, raft.interactions[interaction].get(commandName)) :
+      slashcmd ? this.boat.interactions.commands.set(commandName, raft.interactions.commands.get(commandName)) :
         this.boat.commands.set(commandName, raft.commands.get(commandName));
-
+      
       message.channel.send(`You have succesfully reloaded the command ${commandName}`)
     } catch(error) {
       console.error(error);
