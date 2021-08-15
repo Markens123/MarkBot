49d684c23cd803422cdf469e7548c6ff319dde80
Markens123 <mypepe17@gmail.com>
2021-08-14 22:25:45 -0400

    feat: unlink command

--- src/rafts/Anime/commands/index.ts
+++ src/rafts/Anime/commands/index.ts
@@ -3,5 +3,6 @@ const commands: any = {};
 commands.link = (await import('./link.js')).default;
 commands.mal = (await import('./mal.js')).default;
 commands.sauce = (await import('./sauce.js')).default;
+commands.unlink = (await import('./unlink.js')).default;
 
 export default commands;
