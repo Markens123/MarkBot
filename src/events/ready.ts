972382dca3f761c90060eb68c540226648209215
Markens123 <mypepe17@gmail.com>
2021-08-28 14:36:14 -0400

    Stuff

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,16 @@
 
 import { fileURLToPath } from 'url';
+import { BoatI } from '../../lib/interfaces/Main';
 var module = fileURLToPath(import.meta.url);
 
 
 
-export default boat => {
+export default (boat: BoatI) => {
+  const client = boat.client;
+
   boat.log(module, 'Connected to discord!');
-  boat.client.channels.fetch('807033695483461632');
+  client.channels.fetch('807033695483461632');
+
+  client.reminders.clear()
+
 };
