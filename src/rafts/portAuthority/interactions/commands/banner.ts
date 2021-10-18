aa23dd925bf2b70519c4f8982f0610cad26603ba
Markens123 <46985423+Markens123@users.noreply.github.com>
2021-10-18 11:52:29 -0400

    Add banner command

---
+++ src/rafts/portAuthority/interactions/commands/banner.ts
@@ -0,0 +1,42 @@
+import { CommandInteraction, MessageEmbed } from 'discord.js';
+import BaseInteraction from '../../../BaseInteraction.js';
+
+const definition = {
+  name: 'banner',
+  description: 'Gets the banner of any user',
+  options: [
+    {
+        name: "user",
+        description: "User to get the banner from.",
+        type: 6,
+        required: true
+    }
+    ]  
+};
+
+class BannerInteraction extends BaseInteraction {
+  constructor(boat) {
+    const info = {
+      name: 'avatar',
+      enabled: true,      
+      definition,
+    };
+    super(boat, info);
+  }
+
+  async run(interaction: CommandInteraction, args: any) {
+    const client = this.boat.client;
+    let user = args?.find(arg => arg.name === `user`)?.value;
+
+    let res = await client.users.fetch(user, {force: true}).then(myUser => myUser).catch(error => error);
+
+    let embed = new MessageEmbed()
+      .setTitle(`${res.tag}'s banner'`)
+      .setColor('BLACK')
+      .setImage(res.bannerURL({format: 'png' ,size: 256, dynamic: true}));
+
+    interaction.reply(res);
+  }
+}
+
+export default BannerInteraction;
