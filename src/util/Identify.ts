08ad59701a91fdefafac7c04d7b7c4c6cfd3ef1c
Markens123 <mypepe17@gmail.com>
2021-11-22 21:45:59 -0500

    add python3 for linux

--- src/util/Identify.ts
+++ src/util/Identify.ts
@@ -1,7 +1,7 @@
 import { exec } from 'child_process';
 
 export default (boat, url, callback) => {
-    let proc = exec(`python src/scripts/resnext.py ${url}`, { timeout: 20000 }, (error, stdout, stderr) => {
+    let proc = exec(`python${process.env.P3 ? '3':''} src/scripts/resnext.py ${url}`, { timeout: 20000 }, (error, stdout, stderr) => {
         if (!stdout) {
             exec(`kill ${proc.pid}`, (killError, killStdout, killStderr) => {
 
