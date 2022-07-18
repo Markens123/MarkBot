1419c48aa99aef2411186a889bf764c9e135a358
Markens123 <mypepe17@gmail.com>
2022-07-18 18:16:11 -0400

    Add reponse when deleting a message that isn't urs

--- src/rafts/portAuthority/interactions/buttonComponents/delete.ts
+++ src/rafts/portAuthority/interactions/buttonComponents/delete.ts
@@ -23,6 +23,8 @@ class DeleteInteraction extends BaseInteraction {
       if (interaction.message.flags.toArray().includes('Ephemeral')) return interaction.reply({ content: "You can't delete an ephemeral message silly but you can dismiss it by clicking 'Dismiss Message' below", ephemeral: true })
     
       return interaction.channel.messages.cache.get(interaction.message.id)?.delete().catch(() => {});
+    } else {
+      return interaction.reply({ content: "You can't delete this message", ephemeral: true })
     }
   }
 
