63c28bbbb31845f94ca6ceeb0542402d5930336c
Markens123 <mypepe17@gmail.com>
2022-07-28 17:13:50 -0400

    Add pm as default

--- src/rafts/portAuthority/interactions/commands/timestamp.ts
+++ src/rafts/portAuthority/interactions/commands/timestamp.ts
@@ -15,9 +15,14 @@ class TimestampInteraction extends BaseInteraction {
 
   async run(interaction: CommandInteraction) {
     const date = interaction.options.get('date', true).value.toString().replace('rd ', ' ').replace('st ', ' ').replace('th ', ' ').replace('nd ', ' ');
-    const time = interaction.options.get('time', true).value.toString();
+    let time = interaction.options.get('time', true).value.toString();
     const tz = interaction.options.get('timezone', false)?.value.toString() ?? 'America/New_York';
     
+    if (!time.toLocaleLowerCase().includes('pm') && !time.toLocaleLowerCase().includes('am')) {
+      time += ' pm'
+      time = date.trim().replace(/ +(?= )/g,'');
+    }
+
     let string = `${date} ${time}`;
 
     const dt = DateTime.fromFormat(string, "MMM d y t", { zone: tz });
