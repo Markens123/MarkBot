61f0c1256f6f8d8e278178f63b9a2c9312e340f8
Markens123 <mypepe17@gmail.com>
2022-09-16 09:28:35 -0400

    Temp def for tasks until djs fixes types

--- src/rafts/Tasks/interactions/commands/tasks.ts
+++ src/rafts/Tasks/interactions/commands/tasks.ts
@@ -1,6 +1,6 @@
 import BaseInteraction from '../../../BaseInteraction.js';
 import * as util from 'util';
-import { ChatInputCommandInteraction, CommandInteractionOption, ModalBuilder, SlashCommandBuilder, TextInputBuilder, TextInputStyle } from 'discord.js';
+import { ChannelType, ChatInputCommandInteraction, CommandInteractionOption, ModalBuilder, SlashCommandBuilder, TextInputBuilder, TextInputStyle } from 'discord.js';
 import { ModalComponents, ModalFunctions } from '../../../../util/Constants.js';
 
 class TaskInteraction extends BaseInteraction {
@@ -10,13 +10,83 @@ class TaskInteraction extends BaseInteraction {
       enabled: true,
       guild: '816098833054302208',
       subcommands: true,
-      definition: getDefinition(),
+      definition: tempDef(),
     };
     super(boat, info);
   }
 
-  async run(interaction: ChatInputCommandInteraction, args: CommandInteractionOption[]) {
+  async run() {
+    throw new Error(`tasks.ts was ran but should never be run`)
   }
+}
+ 
+function tempDef() { 
+return {
+  options: [
+    {
+      type: 1,
+      name: 'setup',
+      name_localizations: undefined,
+      description: 'Setup tasks for this server',
+      description_localizations: undefined,
+      options: []
+    },
+    {
+      type: 1,
+      name: 'config',
+      name_localizations: undefined,
+      description: 'Edit the tasks config for this server',
+      description_localizations: undefined,
+      options: []
+    },
+    {
+      type: 1,
+      name: 'create',
+      name_localizations: undefined,
+      description: 'Creates a new task',
+      description_localizations: undefined,
+      options: [
+        {
+          channel_types: [ 15 ],
+          name: 'channel',
+          name_localizations: undefined,
+          description: 'Channel to make tasks fourm',
+          description_localizations: undefined,
+          required: false,
+          type: 7
+        }
+      ]
+    },
+    {
+      type: 1,
+      name: 'edit',
+      name_localizations: undefined,
+      description: 'Edits an existing task',
+      description_localizations: undefined,
+      options: [
+        {
+          choices: undefined,
+          autocomplete: undefined,
+          type: 3,
+          name: 'id',
+          name_localizations: undefined,
+          description: 'The task id to edit',
+          description_localizations: undefined,
+          required: false,
+          max_length: undefined,
+          min_length: undefined
+        }
+      ]
+    }
+  ],
+  name: 'tasks',
+  name_localizations: undefined,
+  description: 'Tasks stuff',
+  description_localizations: undefined,
+  default_permission: undefined,
+  default_member_permissions: undefined,
+  dm_permission: undefined
+}
 }
 
 function getDefinition() {
@@ -37,6 +107,13 @@ function getDefinition() {
       subcommand
         .setName('create')
         .setDescription('Creates a new task')
+        .addChannelOption(option =>
+          option
+            .setName('channel')
+            .addChannelTypes(0) //TODO: Make it Channel.Type.Fourm whatever it is when they fix this
+            .setRequired(false)
+            .setDescription('Channel to make tasks fourm')
+        )
     )
     .addSubcommand(subcommand =>
       subcommand
