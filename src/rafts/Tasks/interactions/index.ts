76a2a436c7a04e4e0307b96239f7c89751146a85
Markens123 <46985423+Markens123@users.noreply.github.com>
2022-09-25 00:21:11 -0400

    chore: unused interactions

--- src/rafts/Tasks/interactions/index.ts
+++ src/rafts/Tasks/interactions/index.ts
@@ -4,7 +4,5 @@ interactions.commands = (await import('./commands/index.js')).default;
 interactions.subcommands = (await import('./subcommands/index.js')).default;
 interactions.modals = (await import('./modals/index.js')).default;
 interactions.selectMenuComponents = (await import('./selectMenuComponents/index.js')).default;
-//interactions.buttonComponents = (await import('./buttonComponents/index.js')).default;
-//interactions.messageContextMenuComponents = (await import('./contextMenuComponents/index.js')).messages;
 
 export default interactions;
