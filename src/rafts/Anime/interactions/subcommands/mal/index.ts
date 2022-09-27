325571aeab5f7220c71fe26190973b76fb4fcd4c
Markens123 <mypepe17@gmail.com>
2022-09-27 17:47:20 -0400

    feat(mal): add link and unlink

--- src/rafts/Anime/interactions/subcommands/mal/index.ts
+++ src/rafts/Anime/interactions/subcommands/mal/index.ts
@@ -3,7 +3,7 @@ const commands: any = {};
 commands.mylist = (await import('./mylist.js')).default;
 commands.get = (await import('./get.js')).default;
 commands.search = (await import('./search.js')).default;
-//commands.link = (await import('./link.js')).default;
-//commands.unlink = (await import('./unlink.js')).default;
+commands.link = (await import('./link.js')).default;
+commands.unlink = (await import('./unlink.js')).default;
 
 export default commands;
---
+++ src/rafts/Anime/interactions/subcommands/mal/unlink.ts
@@ -0,0 +1,33 @@
+import { randomUUID } from 'crypto';
+import { ChatInputCommandInteraction, EmbedBuilder } from 'discord.js';
+import { InteractionYesNo } from '../../../../../util/Buttons.js';
+import BaseInteraction from '../../../../BaseInteraction.js';
+
+class UnLinkInteraction extends BaseInteraction {
+  constructor(boat) {
+    const info = {
+      name: 'unlink',
+      enabled: true,
+    };
+    super(boat, info);
+  }
+
+  async run(interaction: ChatInputCommandInteraction) {
+    const client = this.boat.client;
+
+    await interaction.reply({ content: '‎', ephemeral: true })
+
+    if (!client.maldata.get(interaction.user.id)) return interaction.editReply({ content: "Error you don't have an account to unlink."});
+
+    const resp = await InteractionYesNo({ content: 'Are you sure that you want to unlink your account?', interaction, editReply: true})
+
+    if (!resp) return interaction.editReply({ content: 'Unlink operation canceled.', components: [] })
+    else {
+      client.maldata.delete(interaction.user.id);
+      interaction.editReply({ content: "Success, your account has been unlinked.", components: [] });
+    }
+  }
+}
+
+
+export default UnLinkInteraction;
