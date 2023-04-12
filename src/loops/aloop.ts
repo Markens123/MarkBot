157c0e40e9cb3519c291fa2b947ede51366116be
Markens123 <mypepe17@gmail.com>
2023-04-12 19:55:52 -0400

    fix: revert prev commit and fix anime api

--- src/loops/aloop.ts
+++ src/loops/aloop.ts
@@ -1,179 +1,112 @@
-import * as cheerio from 'cheerio';
-import { EmbedBuilder } from 'discord.js';
-import got from 'got';
-import { AnimeI, SimpleAnime } from '../../../../lib/interfaces/Main';
-import { shorten } from '../../../util/Constants.js';
+import { TextChannel } from 'discord.js';
+import Enmap from 'enmap';
+import { LoopOptions, SimpleAnime } from '../../lib/interfaces/Main.js';
+import AnimeAPI from '../rafts/Anime/apis/anime.js';
+import { ChunkEmbeds } from '../util/Constants.js';
+import BaseLoop from './BaseLoop.js';
 
-class AnimeAPI {
-  private url: string = 'https://gogoanime.news/recent-release-anime';
-  private api_url: string = 'https://api.myanimelist.net/v2/anime';
-
-
-  async getLatest(count: number = 20): Promise<SimpleAnime[]> {
-
-    if (!process.env.MAL_CLIENT_ID) {
-      return null
-    }
-
-    const $ = cheerio.load(await this.getHTML());
-
-    let arr: { title: string, episode: number, url: string }[] = [];
-
-    let farr = [];
-
-    const list = $('#wrapper_bg > section > div > div.page_content > ul').children();
-
-    list.each((i, e) => {
-      let ii = i + 1;
-      const title = $(e).find(`li:nth-child(${ii}) > div.name > a`).attr('title') || $(e).find(`li:nth-child(${ii}) > div.name > a > h4`).text();
-      if (title) {
-        arr.push({
-          title,
-          episode: parseEps($(e).find(`li:nth-child(${ii}) > p`).text().trim()),
-          url: `https://animension.to/search?search_text=${encodeURI(title)}`
-        })
-      }
-    });
-
-    for (let i = 0; i < arr.length; i++) {
-      const anime = await this.getRawVideoData(arr[i].title)
-      if (anime) {
-        const simple = this.toSimple(anime, arr[i].episode, arr[i].url);
-
-        farr.push(simple);
-      }
-    }
-
-    return farr.slice(0, count);
-  }
-
-
-  async getHTML(): Promise<string> {
-    const { body } = await got.get(this.url);
-    return body;
-  }
-
-  toSimple(og: AnimeI, eps: number | null, url?: string): SimpleAnime {
-    return {
-      id: og.id.toString(),
-      title: og.title,
-      mal_url: `https://myanimelist.net/anime/${og.id}`,
-      url,
-      image: og.main_picture.medium,
-      eps,
-      alt_titles: Object.values(og.alternative_titles)?.flat() ?? [],
-      genres: og.genres?.map(x => x.name) ?? [],
-      type: og.media_type,
-      status: og.status
+class ALoop extends BaseLoop {
+  constructor(boat) {
+    const options: LoopOptions = {
+      name: 'animeloop',
+      active: true,
+      time: '0 */3 * * *',
+      dev: false
     };
+    super(boat, options);
   }
 
-  async getRawVideoData(title: string): Promise<AnimeI | undefined> {
-    const searchParams = new URLSearchParams([
-      ['q', title.substring(0, 30)],
-      ['limit', '10'],
-      ['fields', 'id,title,main_picture,media_type,genres,alternative_titles,status'],
-      ['nsfw', 'true'],
-    ]);
+  async run() {
+    const client = this.boat.client;
 
-    const { body } = await got(this.api_url, {
-      //@ts-expect-error typescript is being dumb :P
-      searchParams,
-      headers: {
-        "X-MAL-CLIENT-ID": process.env.MAL_CLIENT_ID
-      }
-    });
+    const oldLatest = client.animealerts.get('latest') as Enmap;
+    const api = new AnimeAPI();
+    const newLatest = await api.getLatest(15);
 
-    const animes = JSON.parse(body).data.map(x => x.node) as AnimeI[];
-
-    const filtered = animes.filter(anime => {
-      const titles = [anime.title.toLowerCase(), ...Object.values(anime.alternative_titles).flat().map(x => x.toLowerCase())];
-      return titles.includes(title.toLowerCase())
-    });
-
-    if (animes?.length === 0) return undefined
-    else if (filtered.length != 0) return filtered[0]
-    else return animes[0]
-  }
-
-  async search(title: string, count: number = 10): Promise<AnimeI[] | undefined> {
-    const searchParams = new URLSearchParams([
-      ['q', title.substring(0, 30)],
-      ['limit', '10'],
-      ['fields', 'id,title,main_picture,media_type,genres,alternative_titles,status'],
-      ['nsfw', 'true'],
-    ]);
-
-    const { body } = await got(this.api_url, {
-      //@ts-expect-error typescript is being dumb :P
-      searchParams,
-      headers: {
-        "X-MAL-CLIENT-ID": process.env.MAL_CLIENT_ID
-      }
-    });
-
-    const animes = JSON.parse(body).data.map(x => x.node) as AnimeI[];
-
-    const filtered = animes.filter(anime => {
-      const titles = [anime.title.toLowerCase(), ...Object.values(anime.alternative_titles).flat().map(x => x.toLowerCase())];
-      return titles.includes(title.toLowerCase())
-    });
-
-    if (animes?.length === 0) return undefined
-    else if (filtered.length != 0) return filtered.slice(0, count)
-    else return animes.slice(0, count)
-  }
-
-  async getAnime(id: string): Promise<AnimeI | undefined> {
-    const searchParams = new URLSearchParams([
-      ['fields', 'id,title,main_picture,media_type,genres,alternative_titles,status'],
-      ['nsfw', 'true'],
-    ]);
-
-    const { body } = await got(`${this.api_url}/${id}`, {
-      //@ts-expect-error typescript is being dumb :P
-      searchParams,
-      headers: {
-        "X-MAL-CLIENT-ID": process.env.MAL_CLIENT_ID
-      }
-    }).catch(_ => { return { body: null } });
-
-    if (!body) return undefined;
-
-    return JSON.parse(body) as AnimeI;
-  }
-
-  genEmbeds(data: SimpleAnime[]): EmbedBuilder[] {
-    let arr = [];
-    for (let i = 0; i < data.length; i++) {
-      arr.push(
-        new EmbedBuilder()
-          .setTitle(`New Episode for ${data[i].alt_titles[0] || data[i].title}`)
-          .setDescription(`[Episode ${data[i].eps}](${data[i].url})\n[MAL Url](${data[i].mal_url})`)
-          .setImage(data[i].image)
-          .setColor('Random')
-          .setFooter({ text: `${data[i].type.toUpperCase()} • ${shorten(data[i].genres.join(' • '), 2000, ' • ')}` })
-      )
+    if (!newLatest) {
+      return this.boat.log.error('Animeloop', 'New anime not found')
     }
-    return arr;
-  }
 
-  genDemoEmbed(data: SimpleAnime): EmbedBuilder {
-    return new EmbedBuilder()
-      .setTitle(data.title)
-      .setDescription(`[MAL Url](${data.mal_url})`)
-      .setImage(data.image)
-      .setColor('Random')
-      .setFooter({ text: `${data.type.toUpperCase()} • ${shorten(data.genres.join(' • '), 2000, ' • ')}` })
+    type list = { id: string, eps: number }[];
+
+    let diff: list = [];
+    let oldArr: list = [];
+    let newArr: list = [];
+
+    oldArr = Object.keys(oldLatest).map((x) => ({ id: oldLatest[x].id, eps: oldLatest[x].eps }));
+    newArr = newLatest.map(x => ({ id: x.id, eps: x.eps }));
+
+    for (let i = 0; i < newArr.length; i++) {
+
+      const filtered = oldArr.filter(x => x.id === newArr[i].id);
+
+      if (filtered.length === 0) {
+        diff.push(newArr[i])
+      } else {
+        if (filtered[0].eps < newArr[i].eps) {
+          diff.push(newArr[i])
+        }
+      }
     }
+
+    if (diff.length) {
+      const newAnime = newLatest.map(x => diff.filter(y => y.id === x.id).length ? x : null).filter(x => x !== null);
+
+      client.animealerts.forEach(async (g, i) => {
+        if (i !== 'latest') {
+
+          const animeToPost = newAnime.filter(x => g.animes.some(i => x.id == i));
+          if (animeToPost.length != 0) {
+
+            const embeds = api.genEmbeds(animeToPost);
+            const channel = await client.channels.fetch(g.channel) as TextChannel;
+            let ids = [];
+
+            for (const p in g.mentions) {
+              const search = animeToPost.filter(x => x.id == p)
+              if (search.length != 0) {
+                ids.push(...g.mentions[p])
+              }
+            }
+
+            const mentions = ids.join(' ');
+
+            ChunkEmbeds(embeds, async (c_embeds) => {
+              await channel.send({ content: mentions || null, embeds: c_embeds })
+            })
+
+            const ids_rmv = animeToPost.map(x => x.status == 'finished_airing' ? x.id : null).filter(x => x !== null);
+
+            const ids_cur = client.animealerts.get(i).animes.filter(x => !ids_rmv.includes(x));
+            client.animealerts.set(i, ids_cur, 'animes');
+
+            const men_cur = client.animealerts.get(i).mentions;
+
+            const filtered = Object.keys(men_cur)
+              .filter(key => !ids_rmv.includes(key))
+              .reduce((obj, key) => {
+                obj[key] = men_cur[key];
+                return obj;
+              }, {});
+
+            client.animealerts.set(i, filtered, 'mentions');
+
+          }
+        }
+      })
+
+      newLatest.forEach(anime => {
+        client.animealerts.set('latest', anime, anime.id.toString())
+      })
+    }
+
+    const asArray = Object.entries(client.animealerts.get('latest'));
+    const filtered = asArray.filter(([_, value]: [string, SimpleAnime]) => value.status != 'finished_airing');
+    const final = Object.fromEntries(filtered);
+    client.animealerts.set('latest', final);
+
+    return;
+  }
 }
 
-function parseEps(str: string): number {
-  const ep = str.replace('Episode: ', '');
-  const num = parseInt(ep);
-
-  if (!num) return 1
-  else return num
-}
-
-export default AnimeAPI;
+export default ALoop;
